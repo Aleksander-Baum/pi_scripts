d5cda5a4d62f3c2fdbd6dabdd865da7e88a7dece
Aleksander-Baum <r0930134@student.thomasmore.be>
2023-03-15 13:39:16 +0100

    another test

--- stepper.py
+++ stepper.py
@@ -1,38 +1,37 @@
-from pyA20.gpio import gpio
-from pyA20.gpio import port
 import time
+import wiringpi
+import sys
 
 # GPIO pins connected to the stepper motor driver
-step_pin = port.PA1
-dir_pin = port.PA0
+STEP_PIN = 0
+DIR_PIN = 1
+
+# Set up the WiringPi library
+wiringpi.wiringPiSetup()
 
 # Set up the GPIO pins as output
-gpio.init()
-gpio.setcfg(step_pin, gpio.OUTPUT)
-gpio.setcfg(dir_pin, gpio.OUTPUT)
+wiringpi.pinMode(STEP_PIN, wiringpi.OUTPUT)
+wiringpi.pinMode(DIR_PIN, wiringpi.OUTPUT)
 
 # Set the direction of the stepper motor
-gpio.output(dir_pin, gpio.HIGH)
+wiringpi.digitalWrite(DIR_PIN, wiringpi.HIGH)
 
 # Define the number of steps per revolution and delay between steps
 steps_per_revolution = 200
-delay = 0.005
+delay = 0.01
 
 # Function to move the stepper motor a specified number of steps
 def move_steps(num_steps):
     # Move the motor one step at a time in the specified direction
     for i in range(num_steps):
-        gpio.output(step_pin, gpio.HIGH)
+        wiringpi.digitalWrite(STEP_PIN, wiringpi.HIGH)
         time.sleep(delay)
-        gpio.output(step_pin, gpio.LOW)
+        wiringpi.digitalWrite(STEP_PIN, wiringpi.LOW)
         time.sleep(delay)
 
 # Move the stepper motor 200 steps in one direction
 move_steps(steps_per_revolution)
 
 # Move the stepper motor 200 steps in the opposite direction
-gpio.output(dir_pin, gpio.LOW)
+wiringpi.digitalWrite(DIR_PIN, wiringpi.LOW)
 move_steps(steps_per_revolution)
-
-# Clean up the GPIO pins
-gpio.cleanup()
