4110c1c5167309886cb2b41001280fc77bd4ac0b
Aleksander-Baum <r0930134@student.thomasmore.be>
2023-03-22 15:20:03 +0100

    2 pot meters working

--- Ch6_MCP3008_demo_2pot.py
+++ Ch6_MCP3008_demo_2pot.py
@@ -30,7 +30,11 @@ try:
         ActivateADC()
         tmp0 = readadc(0) # read channel 0
         DeactivateADC()
+        ActivateADC()
+        tmp1 = readadc(1) # read channel 0
+        DeactivateADC()
         print ("input0:",tmp0)
+        print ("input1:",tmp1)
         time.sleep(0.2)
 
 except KeyboardInterrupt:
