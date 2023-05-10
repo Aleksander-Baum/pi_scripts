303504db17125f5016d9cd64c6b8e852f7cbd7e0
Aleksander-Baum <r0930134@student.thomasmore.be>
2023-05-10 18:14:16 +0200

    Project om al 14 te hebben

--- Project/project.py
+++ Project/project.py
@@ -1,13 +1,36 @@
 import wiringpi
 import time
+import json
+import requests
+from ch7_ClassLCD import LCD
+
+def ActivateLCD():
+    wiringpi.digitalWrite(pin_CS_lcd, 0)       # Actived LCD using CS
+    time.sleep(0.000005)
+
+def DeactivateLCD():
+    wiringpi.digitalWrite(pin_CS_lcd, 1)       # Deactived LCD using CS
+    time.sleep(0.000005)
+
+PIN_OUT     =   {  
+                'SCLK'  :   14,
+                'DIN'   :   11,
+                'DC'    :   9, 
+                'CS'    :   15, #We will not connect this pin! --> we use w13
+                'RST'   :   10,
+                'LED'   :   6, #backlight   
+}
 
 TRIG_PIN = 0
 ECHO_PIN = 1
 MOTOR_PIN_1 = 3
 MOTOR_PIN_2 = 4
-MOTOR_PIN_3 = 6
-MOTOR_PIN_4 = 9
-pinLed = 10
+MOTOR_PIN_3 = 12
+MOTOR_PIN_4 = 7
+pinLed = 5
+pinSwitch1 = 2
+pinSwitch2 = 15
+pin_CS_lcd = 13
 
 wiringpi.wiringPiSetup()
 wiringpi.pinMode(TRIG_PIN, wiringpi.OUTPUT)
@@ -19,6 +42,15 @@ wiringpi.pinMode(MOTOR_PIN_4, wiringpi.OUTPUT)
 WAVE_DRIVE_SEQUENCE = [[0,0,0,1], [0,0,1,0], [0,1,0,0], [1,0,0,0]]
 RWAVE_DRIVE_SEQUENCE = [[1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1]]
 wiringpi.pinMode(pinLed, 1)
+wiringpi.pinMode(pinSwitch1, 0)
+wiringpi.pinMode(pinSwitch2, 0)
+wiringpi.pinMode(pin_CS_lcd , 1)            # Set pin to mode 1 ( OUTPUT )
+url = "http://muizenval.hub.ubeac.io/iotessaleksanderbaum"
+uid = "iotessaleksanderbaum"
+
+ActivateLCD()
+lcd_1 = LCD(PIN_OUT)
+
 
 def distance():
     wiringpi.digitalWrite(TRIG_PIN, wiringpi.HIGH)
@@ -47,26 +79,100 @@ def stepMotor(step, mode):
 
 # Test the distance function
 
-sensor_on = True
-ledcount = 0
-while sensor_on == True:
+current_time = time.localtime()
+current_hour = str(f"{current_time.tm_hour}:{current_time.tm_min}")
+
+trapclosed = False
+trapstatus = 0
+lcd_1.clear()
+lcd_1.set_backlight(0)
+triggercount = 0
+while True:
     dist = distance()
-    while dist >= 35:
+    while dist >= 35 and trapclosed == False:
+        ActivateLCD()
+        lcd_1.clear()
+        lcd_1.go_to_xy(0, 0)
+        lcd_1.put_string(current_hour + '\nStatus: armed' + '\nTrigger count: ' + str(triggercount))
+        lcd_1.refresh()
+        DeactivateLCD()
+        time.sleep(0.5)
         dist = distance()
         print(f"Measured Distance: {dist:.2f} cm")
-        time.sleep(1.5)
-    else:
+        time.sleep(1)
+        if(wiringpi.digitalRead(pinSwitch1) == 0):
+            triggercount += 1
+            trapstatus = 1
+            ActivateLCD()
+            lcd_1.clear()
+            lcd_1.go_to_xy(0, 0)
+            lcd_1.put_string(current_hour + '\nStatus: triggered' + '\nTrigger count: ' + str(triggercount))
+            lcd_1.refresh()
+            DeactivateLCD()
+            trapclosed = True
+            time.sleep(0.3)
+            wiringpi.digitalWrite(pinLed,0)
+            for i in range(200):
+                stepMotor(i % 4, 'wave')
+                time.sleep(0.01)  # wait 10ms between steps
+    if dist < 35:
+        triggercount += 1
+        trapstatus = 1
+        ActivateLCD()
+        lcd_1.clear()
+        lcd_1.go_to_xy(0, 0)
+        lcd_1.put_string(current_hour + '\nStatus: triggered' + '\nTrigger count: ' + str(triggercount))
+        lcd_1.refresh()
+        DeactivateLCD()
         for i in range(200):
             stepMotor(i % 4, 'wave')
             time.sleep(0.01)  # wait 10ms between steps
-        while ledcount <= 5:
-            wiringpi.digitalWrite(pinLed, 0)
-            time.sleep(0.5)
-            wiringpi.digitalWrite(pinLed, 1)
-            time.sleep(0.5)
-            ledcount += 1
-        for j in range(200):
-            stepMotor(j % 4, 'rwave')
-            time.sleep(0.01)
-            wiringpi.digitalWrite(pinLed, 1)
-        sensor_on = False
+            trapclosed = True
+    while trapclosed == True:
+        data= {
+            "id": uid,
+            "sensors":[{
+                'id': 'trapstatus',
+                'data': trapstatus
+            },
+            {   'id': 'triggercount',
+                'data': triggercount
+            }]
+        }
+        r = requests.post(url, verify=False, json=data)
+        ActivateLCD()
+        lcd_1.clear()
+        lcd_1.go_to_xy(0, 0)
+        lcd_1.put_string(current_hour + '\nStatus: triggered' + '\nTrigger count: ' + str(triggercount))
+        lcd_1.refresh()
+        DeactivateLCD()
+        wiringpi.digitalWrite(pinLed, 0)
+        time.sleep(0.2)
+        wiringpi.digitalWrite(pinLed, 1)
+        time.sleep(0.2)
+        if(wiringpi.digitalRead(pinSwitch2) == 0):
+            trapstatus = 0
+            data= {
+                "id": uid,
+                "sensors":[{
+                    'id': 'trapstatus',
+                    'data': trapstatus
+                },
+                {   'id': 'triggercount',
+                    'data': triggercount
+                }]
+            }
+            r = requests.post(url, verify=False, json=data)
+            ActivateLCD()
+            lcd_1.clear()
+            lcd_1.go_to_xy(0, 0)
+            lcd_1.put_string(current_hour + '\nStatus: armed' + '\nTrigger count: ' + str(triggercount))
+            lcd_1.refresh()
+            DeactivateLCD()
+            trapclosed = False
+            time.sleep(0.3)
+            wiringpi.digitalWrite(pinLed,1)
+            for j in range(200):
+                stepMotor(j % 4, 'rwave')
+                time.sleep(0.01)
+                wiringpi.digitalWrite(pinLed, 1)
